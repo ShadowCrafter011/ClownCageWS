a27be96a66b25ca4ec50dc63ab35cd8e6e427fa3
Lukas Köllner <lkoe@bluewin.ch>
2023-09-11 22:26:53 +0000

    Added web notification on consumer creation

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import consumer from "../channels/consumer"
+import $ from "jquery"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
@@ -14,6 +15,7 @@ export default class extends Controller {
   reload(data) {
     if (data.action != "reload_navbar") return
     this.stimulus.element.reload()
+    bootstrap.Toast.getOrCreateInstance($("#new-consumer-toast")).show()
   }
 
   disconnect() {
